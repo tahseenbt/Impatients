2dc45ff2fcba63e345b813c11e88e18783d901c6
rgibbons-test <rgibbons1221@gmail.com>
2020-02-01 14:39:26 -0500

    another small syntax fix for avg_wait()

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ NUM_SERVED = 0
 
 #calculates the average wait time, increments the number of patients served
 #cur_time: time at the moment of the call minus the elapsed time of the session
-def AVG_WAIT()
+def avg_wait():
     cur_time = datetime.datetime.now() - (NUM_SERVED + AVG_TIME) #note: convert to string?
     AVG_TIME = (NUM_SERVED* AVG_TIME + cur_time) / (NUM_SERVED + 1)
     NUM_SERVED+=1
