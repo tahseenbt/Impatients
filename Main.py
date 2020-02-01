67aaf65ff442cc3e50bd7be24ed6e58335e8ca1d
rgibbons-test <rgibbons1221@gmail.com>
2020-02-01 14:47:59 -0500

    resolved global variable issue with AVG_TIME, NUM_SERVED

--- Main.py
+++ Main.py
@@ -9,6 +9,8 @@ NUM_SERVED = 0
 #calculates the average wait time, increments the number of patients served
 #cur_time: time at the moment of the call minus the elapsed time of the session
 def avg_wait():
+    global AVG_TIME
+    global NUM_SERVED
     cur_time = datetime.datetime.now() - (NUM_SERVED + AVG_TIME) #note: convert to string?
     AVG_TIME = (NUM_SERVED* AVG_TIME + cur_time) / (NUM_SERVED + 1)
     NUM_SERVED+=1
