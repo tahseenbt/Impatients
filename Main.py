38287d1652961e3f2a326528d73a91542bc6bf37
rgibbons-test <rgibbons1221@gmail.com>
2020-02-01 14:24:56 -0500

    added AVG_TIME() global function, used to update the AVG_TIME, NUM_SERVED global variables

--- Main.py
+++ Main.py
@@ -1,6 +1,14 @@
+import datetime
+
 TICKET_LIST = []
 
 AVG_TIME = 0
 
 NUM_SERVED = 0
 
+#calculates the average wait time, increments the number of patients served
+#cur_time: time at the moment of the call minus the elapsed time of the session
+def AVG_WAIT()
+    cur_time = datetime.datetime.now() - (NUM_SERVED + AVG_TIME) #note: convert to string?
+    AVG_TIME = (NUM_SERVED* AVG_TIME + cur_time) / (NUM_SERVED + 1)
+    NUM_SERVED+=1
