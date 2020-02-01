426c5f0aa2f7696d36594ea48b6f9458fbd597cc
jidicula <johanan.idicula@gmail.com>
2020-02-01 17:16:18 -0500

    Fix time calculations
    Resolves #12

--- Main.py
+++ Main.py
@@ -14,13 +14,20 @@ CURRENT_ID = 0
 # cur_time: time at the moment of the call minus the elapsed time of the session
 def avg_wait():
     global AVG_TIME
-    global NUM_SERVED
     cur_time = datetime.datetime.now() - datetime.timedelta(
         minutes=(NUM_SERVED * AVG_TIME))
-    AVG_TIME = (NUM_SERVED * AVG_TIME + cur_time) / (NUM_SERVED + 1)
-    NUM_SERVED += 1
+    AVG_TIME = (NUM_SERVED * AVG_TIME + cur_time.minute) / (NUM_SERVED + 1)
 
 
 def serve():
+    global NUM_SERVED
     avg_wait()
     TICKET_LIST.remove(TICKET_LIST[0])
+    NUM_SERVED += 1
+
+
+if __name__ == "__main__":
+    NUM_SERVED = 3
+    print(AVG_TIME)
+    avg_wait()
+    print(AVG_TIME)
