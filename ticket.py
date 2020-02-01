d10a0b58779e7a8bd4be75342daaeffedeb37ade
rgibbons-test <rgibbons1221@gmail.com>
2020-02-01 16:50:37 -0500

    added "est_time" attribute and "est_update()" for ticket class, the former calls the latter resolves #3 resolves #4

--- ticket.py
+++ ticket.py
@@ -13,7 +13,15 @@ class Ticket:
         self.t_id = CURRENT_ID
         TICKET_LIST.append(self.t_id)
 
-        
+    #iniitialize to estimated time start at t=end of queue
+        global AVG_TIME
+        self.est_update()
+
+    # gives an updated eta for patient to be served
+    # TICKET_LIST.getIndex(self.id) is new position of this ticket
+    def est_update(self):
+        global AVG_TIME
+        self.t_est_time = TICKET_LIST.index(self.t_id) * AVG_TIME
     
     def quit(self):
         TICKET_LIST.remove(self.t_id)
@@ -23,11 +31,12 @@ if __name__ == "__main__":
     a = Ticket()
     b = Ticket()
     print(a.t_id)
-    print(b.t_id)
-    a.quit()
-    b.quit()
+    print(b.t_est_time)
+    #a.quit()
+    #b.quit()
     c = Ticket()
     print(TICKET_LIST)
-    print(a.t_id)
-    print(b.t_id)
-    print(c.t_id)
+    a.quit()
+    #print(a.t_id)
+    print(b.t_est_time)
+    print(c.t_est_time)
