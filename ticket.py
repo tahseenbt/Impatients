61669fdd630ac00ea2931b62e69328e32405c699
Tahseen Bin Taj <tbt@tahseens-MacBook-Pro.local>
2020-02-01 16:37:43 -0500

    quit method implemented

--- ticket.py
+++ ticket.py
@@ -4,14 +4,30 @@ class Ticket:
 
     # Instance attributes
     def __init__(self):
-        
+
+        global CURRENT_ID
+
+        CURRENT_ID += 1
+
         # expensive route:
-        if len(TICKET_LIST) == 0:
-            self.t_id = 1
-        else:
-            self.t_id = TICKET_LIST[-1]+1
+        self.t_id = CURRENT_ID
+        TICKET_LIST.append(self.t_id)
+
+        
+    
+    def quit(self):
+        TICKET_LIST.remove(self.t_id)
 
 
 if __name__ == "__main__":
     a = Ticket()
+    b = Ticket()
     print(a.t_id)
+    print(b.t_id)
+    a.quit()
+    b.quit()
+    c = Ticket()
+    print(TICKET_LIST)
+    print(a.t_id)
+    print(b.t_id)
+    print(c.t_id)
