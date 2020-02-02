8a53405c28ba40e0507270bf5d8e95b089c51fcd
jidicula <johanan.idicula@gmail.com>
2020-02-02 18:02:00 -0500

    Add Tkinter windowing frontend
    Proof of concept windowing, useful for testing now.
    
    What works:
    - New ticket creation from the kiosk
    
    **Known Issues**
    - Dropping out of the queue does not work. Possibly an issue with
    updating the list, more likely to be an issue with Tkinter windows not
    updating
    - Next ticket functionality is untested.
    - Average time remains unimplemented.
    
    resolves #8, resolves #6, resolves #5

--- Main.py
+++ Main.py
@@ -1,1 +1,100 @@
-TICKET_LIST = []
+import ticket
+from functools import partial
+from tkinter import Tk, Button, Label
+
+
+def create_ticket(ticket_list):
+    new_ticket = ticket.Ticket()
+    ticket_list.append(new_ticket)
+    ticket_list[-1].est_update(len(ticket_list) - 1)
+    return new_ticket
+
+
+def digital_ticket(ticket_list):
+    def refresh_time_label():
+        est_time_label.config(
+            text="Estimated time: {0} minutes".format(new_ticket.avg_time))
+        est_time_label.after(1000, refresh_time_label)
+
+    new_ticket = create_ticket(ticket_list)
+    ticket_window = Tk()
+    ticket_window.title("Ticket {0}".format(new_ticket.t_id))
+    ticket_window.geometry("200x200")
+
+    est_time_label = Label(ticket_window,
+                           text="Estimated time: {0} minutes".format(
+                               new_ticket.t_est_time))
+    est_time_label.pack(side="top")
+    refresh_time_label()
+    leave_btn = Button(ticket_window,
+                       text="Leave queue",
+                       bd="5",
+                       command=combine_funcs(
+                           partial(queue_quit, ticket_list,
+                                   len(ticket_list) - 1),
+                           ticket_window.destroy))
+    leave_btn.pack(side="top")
+    ticket_window.mainloop()
+
+
+def queue_quit(ticket_list, ticket_idx):
+    # todo list comprehension for all tickets after ticket_idx
+    ticket_list.pop(ticket_idx)
+
+
+def print_ticket(ticket_list):
+    create_ticket(ticket_list)
+    pass
+
+
+def next_ticket(ticket_list):
+    ticket_list.remove(ticket_list[0])
+    current_ticket_label.config(
+        text="Serving: {0}".format(str(ticket_list[0].t_id)))
+    pass
+
+
+def combine_funcs(*funcs):
+    def combined_func(*args, **kwargs):
+        for f in funcs:
+            f(*args, **kwargs)
+
+    return combined_func
+
+
+# if __name__ == '__main__':
+
+ticket_list = []
+
+# Reception window
+reception = Tk()
+reception.title("Reception")
+reception.geometry('200x200')
+
+# label gets refreshed when ticket is served
+current_ticket_label = Label(reception, text="Serving: 1")
+current_ticket_label.pack(side="top")
+next_ticket_btn = Button(reception,
+                         text="Next ticket",
+                         bd="5",
+                         command=partial(next_ticket, ticket_list))
+next_ticket_btn.pack(side="top")
+
+# Kiosk window
+kiosk = Tk()
+kiosk.title("Kiosk")
+kiosk.geometry('200x200')
+print_btn = Button(kiosk,
+                   text="Print ticket",
+                   bd="5",
+                   command=partial(print_ticket, ticket_list))
+print_btn.pack(side="top")
+digital_ticket_btn = Button(kiosk,
+                            text="Digital ticket",
+                            bd="5",
+                            command=partial(digital_ticket, ticket_list))
+digital_ticket_btn.pack(side="top")
+
+# All mainloops here
+reception.mainloop()
+kiosk.mainloop()
--- ticket.py
+++ ticket.py
@@ -1,4 +1,5 @@
-from Main import *
+# coding: utf-8
+
 
 class Ticket:
     current_id = 1
@@ -6,7 +7,7 @@ class Ticket:
     num_served = 0
 
     # Instance attributes
-    def __init__(self, l,time):
+    def __init__(self):
         self.t_id = Ticket.current_id
         Ticket.current_id += 1
         self.t_est_time = 3
@@ -14,18 +15,15 @@ class Ticket:
     # gives an updated eta for patient to be served
     # TICKET_LIST.getIndex(self.id) is new position of this ticket
     def est_update(self, i):
-        self.t_est_time = (i+1) * avg_time
-    
-    def quit(self):
-        TICKET_LIST.remove(self.t_id)
+        self.t_est_time = (i + 1) * Ticket.avg_time
 
 
 if __name__ == "__main__":
     a = Ticket()
     b = Ticket()
-    #a.quit()
-    #b.quit()
+    # a.quit()
+    # b.quit()
     c = Ticket()
     print(c.t_id)
-    #a.quit()
-    #print(b.t_id)
+    # a.quit()
+    # print(b.t_id)
