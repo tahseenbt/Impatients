d6807a750c5c4481a4c48af828e6a6c76fa8e6e4
Tahseen Bin Taj <tbt@tahseens-MacBook-Pro.local>
2020-02-01 20:11:10 -0500

    Changed implementation of current_id

--- ticket.py
+++ ticket.py
@@ -1,42 +1,35 @@
 from Main import *
 
 class Ticket:
+    current_id = 1
 
     # Instance attributes
     def __init__(self):
+        self.t_id = Ticket.current_id
+        Ticket.current_id += 1
 
-        global CURRENT_ID
-
-        CURRENT_ID += 1
-
-        # expensive route:
-        self.t_id = CURRENT_ID
-        TICKET_LIST.append(self.t_id)
-
-    #iniitialize to estimated time start at t=end of queue
+    """#iniitialize to estimated time start at t=end of queue
         global AVG_TIME
         self.est_update()
 
     # gives an updated eta for patient to be served
     # TICKET_LIST.getIndex(self.id) is new position of this ticket
+    # TODO move to queue_utils? will decide
     def est_update(self):
         global AVG_TIME
         self.t_est_time = TICKET_LIST.index(self.t_id) * AVG_TIME
     
+    # TODO move to queue_utils? will decide
     def quit(self):
-        TICKET_LIST.remove(self.t_id)
+        TICKET_LIST.remove(self.t_id)"""
 
 
 if __name__ == "__main__":
     a = Ticket()
     b = Ticket()
-    print(a.t_id)
-    print(b.t_est_time)
     #a.quit()
     #b.quit()
     c = Ticket()
-    print(TICKET_LIST)
-    a.quit()
-    #print(a.t_id)
-    print(b.t_est_time)
-    print(c.t_est_time)
+    print(a.t_id)
+    #a.quit()
+    print(b.t_id)
